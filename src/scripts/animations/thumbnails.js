8a8a10c6491ebf3a6487d44b156b43056d147c44
marcomaza92 <marcoagustinmaza@gmail.com>
2020-11-05 13:19:39 -0300

    fix(search): rearrange thumbnails toggle and search bar

--- src/scripts/animations/thumbnails.js
+++ src/scripts/animations/thumbnails.js
@@ -17,17 +17,21 @@ $(document).ready(() => {
 
   $('.video-toggle-thumbnails').click(() => {
     if ($('.layout-container').hasClass('without-chat')) {
+      $('.video-toggle-thumbnails').toggleClass('button-out');
       $('.layout-container').toggleClass('without-thumbnails');
       $('.layout-container').addClass('without-chat');
       $('.layout-container').toggleClass('without-thumbnails-and-chat');
       $('.content-expand').toggleClass('rotate');
     } else if ($('.layout-container').hasClass('without-thumbnails') && $('.layout-container').hasClass('fifty-chat')) {
+      $('.video-toggle-thumbnails').toggleClass('button-out');
       $('.layout-container').toggleClass('without-thumbnails');
     } else if ($('.layout-container').hasClass('over-thumbnails') && $('.content-expand').hasClass('rotate')) {
+      $('.video-toggle-thumbnails').toggleClass('button-out');
       $('.layout-container').toggleClass('without-thumbnails');
     } else if ($('.layout-container').hasClass('without-thumbnails') && $('.content-expand').hasClass('rotate')) {
       $('.content-expand').removeClass('rotate');
     } else {
+      $('.video-toggle-thumbnails').toggleClass('button-out');
       $('.layout-container').toggleClass('without-thumbnails');
       $('.content-expand').toggleClass('rotate');
     }
